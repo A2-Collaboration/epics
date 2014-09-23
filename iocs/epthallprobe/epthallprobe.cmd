267cf55374d82ec2b5485a58e56a6a26025a03c6
root <root@slowcontrol.online.a2.kph>
2014-09-23 16:01:20 +0200

    Modified EPT-Hall-Probe to work on the Pi and added koshava5 server to screenrc

--- iocs/epthallprobe/epthallprobe.cmd
+++ iocs/epthallprobe/epthallprobe.cmd
@@ -4,7 +4,7 @@ streamApp_registerRecordDeviceDriver
 
 epicsEnvSet "STREAM_PROTOCOL_PATH", "/opt/epics/iocs/epthallprobe/"
 
-drvAsynIPPortConfigure ("TAGG:EPT", "x2s0.office.a2.kph:1337")
+drvAsynIPPortConfigure ("TAGG:EPT", "localhost:1337")
 
 dbLoadRecords "epthallprobe.db","PROTO=epthallprobe_serial.proto,DEV=TAGG:EPT"
 
