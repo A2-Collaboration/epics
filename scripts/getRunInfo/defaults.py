a33c8e773e55489d58d1f87dd70b4bf21e346b6f
Martin Wolfes <wolfes@kph.uni-mainz.de>
2014-10-19 22:25:32 +0200

    new runsets can be used new

---
+++ scripts/getRunInfo/defaults.py
@@ -0,0 +1,53 @@
+from runset import NewRunSet,SimpleText,EpicsEntry,StaticValue,Dummy,EpicsMnp
+
+
+EtaPrime = NewRunSet("Etaprime",80)
+EtaPrime.lines = [
+       [SimpleText("Run XXX - Start: XX:XX End: XX:XX") ],
+       [],
+       [StaticValue("Energy","1604 MeV") , StaticValue("Set-Current","60 nA"), StaticValue("Radiator","10 mu copper")],
+       [StaticValue("Collimator","4 mm") , StaticValue("Target","Hydrogen"), StaticValue("Trigger","CB Esum>550MeV")],
+       [],
+       [EpicsEntry("P2 Rate","BEAM:IonChamber","kHz",1./1000), EpicsEntry("EPT OR","TAGG:EPT:Sector0:TRIG:OR","MHz",1.0/1000000),Dummy()],
+       [EpicsEntry("Faraday","BEAM:FaradayCup","nA",1./1000) , EpicsEntry("Ladder /P2","TAGG:EPT:LadderP2Ratio","",1.0),Dummy()],
+       [EpicsEntry("Interrupt","TRIG:Trigger","kHz",1./1000), EpicsEntry("Livetime","TRIG:TotalLivetime","%",1.0),Dummy()],
+       [EpicsEntry("TPAS CFD","TAPS:BAF:TRIG:CFD:OR","kHz",1./1000), EpicsEntry("TAPS LED1","TAPS:BAF:TRIG:LED1:OR","kHz",1.0/1000),Dummy()],
+       [],
+       [EpicsEntry("CB ESum","CB:CB:TRIG:ESum_Low","kHz",1./1000), EpicsEntry("Fast Clear","TRIG:FastClear","kHz",1.0/1000),EpicsMnp()],
+       [Dummy(),Dummy(),EpicsEntry("M1","TRIG:MULT:M1","kHz",1.0/1000)],
+       [Dummy(),Dummy(),EpicsEntry("M2","TRIG:MULT:M2","kHz",1.0/1000)],
+       [Dummy(),Dummy(),EpicsEntry("M3","TRIG:MULT:M3","kHz",1.0/1000)],
+       [Dummy(),Dummy(),EpicsEntry("M4","TRIG:MULT:M4","kHz",1.0/1000)],
+       [Dummy(),Dummy(),EpicsEntry("M5","TRIG:MULT:M5","kHz",1.0/1000)],
+       [Dummy(),Dummy(),EpicsEntry("M6","TRIG:MULT:M6","kHz",1.0/1000)],
+       [Dummy(),Dummy(),EpicsEntry("M7","TRIG:MULT:M7","kHz",1.0/1000)],
+       [],
+       [SimpleText("Hydrogen Target:") ],
+       [EpicsEntry("Pressure","TARGET:H2:Pcible","mbar"),Dummy(),Dummy()],
+       [SimpleText("Temperature:"), EpicsEntry("Target","TARGET:H2:CLTS","K"),EpicsEntry("Liquifier","TARGET:H2:Tsi","K")],
+       [],
+       [SimpleText("Taps Fan failure status: <check crate!>") ]
+     ]
+
+EtaPrimeTaggeff = NewRunSet("EtaPrime-Taggeff",80)
+EtaPrimeTaggeff.lines = [
+       [SimpleText("Run XXX - Start: XX:XX End: XX:XX") ],
+       [],
+       [StaticValue("Energy","1604 MeV") , StaticValue("Set-Current","00 nA"), StaticValue("Radiator","10 mu copper")],
+       [StaticValue("Collimator","4 mm") , StaticValue("Target","Hydrogen"),   StaticValue("Trigger","CB Esum>550MeV")],
+       [],
+       [EpicsEntry("P2 Rate","BEAM:IonChamber","kHz",1./1000), EpicsEntry("EPT OR","TAGG:EPT:Sector0:TRIG:OR","MHz",1.0/1000000), Dummy()],
+       [EpicsEntry("Faraday","BEAM:FaradayCup","nA",1./1000),  EpicsEntry("Ladder /P2","TAGG:EPT:LadderP2Ratio","",1.0),          Dummy()],
+       [Dummy(),                                               EpicsEntry("LEAD Glass","BEAM:PBGlass","kHz",1.0/1000),            Dummy()],
+       [],
+       [EpicsEntry("Interrupt","TRIG:Trigger","kHz",1./1000),  EpicsEntry("Livetime","TRIG:TotalLivetime","%",1.0),               Dummy()],
+       [],
+       [SimpleText("Hydrogen Target:") ],
+       [EpicsEntry("Pressure","TARGET:H2:Pcible","mbar"),Dummy(),Dummy()],
+       [SimpleText("Temperature:"), EpicsEntry("Target","TARGET:H2:CLTS","K"),EpicsEntry("Liquifier","TARGET:H2:Tsi","K")],
+       [],
+       [SimpleText("Taps Fan failure status: <check crate!>") ]
+     ]
+
+
+RunsetList = [ EtaPrime , EtaPrimeTaggeff ]
--- scripts/getRunInfo/getRunInfo.py
+++ scripts/getRunInfo/getRunInfo.py
@@ -1,61 +1,36 @@
-#!/usr/bin/python
+#!/usr/bin/ipython
 
-from epics import caget
+import defaults
 import sys
-import runset
-from runset import RunSet
-from configobj import ConfigObj
+import os
 
-#configfile = "/home/a2cb/scripts/getRunInfo/runsets.cfg"
-configfile = "runsets.cfg"
+#termwidth = os.popen('stty size', 'r').read().split()[1]
 
-command = ["load", "default"]
+command = []
 
-#theRunset = RunSet(configfile,'default')
+theRunset = defaults.EtaPrime
+
 
 def Clear():
     sys.stderr.write("\x1b[2J\x1b[H")
-
 
 def GenMessage():
     msg = [""]
     msg.append("")
     if  theRunset:
-        msg.append("========= STATUS for experiment:  " + theRunset.Exp +  " ===================")
+        msg.append("========= STATUS for experiment:  " + theRunset.name +  " ===================")
     msg.append("")
-    msg.append("  Commands: load <runset> - load runset from configfile")
+    msg.append("  Commands: load <number> - load runset")
     msg.append("            list          - list available runsets")
-    msg.append("            elog          - generate elog entry")
+    msg.append("            quit          - exit programm...")
+    #msg.append("            elog          - generate elog entry")
     msg.append("")
     return msg
 
 def command_prompt():
     for l in GenMessage():
         print(l)
-    return raw_input("Hit Return to refresh or type 'quit'...   >> ").split()
-
-def list_runsets():
-    cfg = ConfigObj(configfile)
-    #Clear()
-    print 
-    for line in cfg.sections:
-        print line
-    print
-
-def load_runset(runsetstr):
-    cfg = ConfigObj(configfile)
-    if cfg.sections.count(runsetstr) == 0:
-        print
-        print("     Runset does not exist")
-        print
-        return False
-    if cfg.sections.count(runsetstr) > 1:
-        print
-        print("     More than one config for runset " + runsetstr)
-        print
-        return False
-    else:
-         return RunSet(configfile,runsetstr)
+    return raw_input("Hit Return to refresh or type a command.   >> ").split()
 
 Clear()
 #theRunset.PrintLines()
@@ -65,51 +40,53 @@ while ( True):
 
     if not command:
         if theRunset:
+            Clear()
             theRunset.Update()
             Clear()
-            theRunset.PrintLines()
+            theRunset.Print()
         else:
             print
             print("       load proper runset!")
             print
             command = ["list"]
             continue
-
-    elif not command[0].find("fast"):
-        if theRunset:
-            theRunset.Update(1)
-            Clear()
-            theRunset.PrintLines()
-        else:
-            print
-            print("       load proper runset!")
-            print
-            command = ["list"]
-            continue
-
-    elif not command[0].find("quit"):
-        break
 
     elif not command[0].find("list"):
-        list_runsets()
+        print
+        print "Available Runsets:"
+        print
+        for rsnum in range(len(defaults.RunsetList)):
+            print("  " + str(rsnum) + ":   " +defaults.RunsetList[rsnum].name)
 
-    elif not command[0].find("elog"):
-        theRunset.GenElog()
-    
     elif not command[0].find("load"):
         if len(command) == 2:
-            theRunset = load_runset(command[1])
-            if not theRunset:
+            if command[1].isdigit():
+                rsnum = int(command[1]) 
+                if range(len(defaults.RunsetList)).count(rsnum):
+                    theRunset = defaults.RunsetList[int(command[1])]
+                    theRunset.Update()
+                    Clear()
+                    theRunset.Print()
+                else:
+                    print
+                    print "          Runset does not exist"
+                    print
+                    command = ["list"]
+                    continue
+            else:
+                print
+                print "          Provide runset by number"
+                print
                 command = ["list"]
                 continue
-            else:
-                Clear()
-                theRunset.PrintLines()
         else:
-            Clear()
             print
-            print("       provide exactly 1 runset!")
+            print "          Provide exactly one rumset number"
             print
+            command = ["list"]
+            continue
+    elif not command[0].find("quit"):
+        break
     
     else:
         Clear()
