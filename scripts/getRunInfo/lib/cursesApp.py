1c75734978c747a57e7b3445f641d3f097081cec
Martin Wolfes <wolfes@kph.uni-mainz.de>
2014-10-29 19:36:48 +0100

    changed a color in getRunInfo...

--- scripts/getRunInfo/lib/cursesApp.py
+++ scripts/getRunInfo/lib/cursesApp.py
@@ -31,7 +31,7 @@ class NcApp:
         
         curses.start_color()
         curses.init_pair(1,curses.COLOR_RED,curses.COLOR_BLACK)
-        curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLUE)
+        curses.init_pair(2,curses.COLOR_RED,curses.COLOR_YELLOW)
 
         curses.noecho()
         curses.cbreak()
