ccb99412ad79fbd3de821651af582376215804ef
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-07-06 10:22:29 +0200

    Make NMR readout work with new serial device

--- iocs/daq-master/scripts/MonitorNMR.pl
+++ iocs/daq-master/scripts/MonitorNMR.pl
@@ -9,19 +9,20 @@ use CA;
 my $chan = CA->new("TAGG:MagneticField.A");
 CA->pend_io(3);
 
-my $t = new Net::Telnet (Timeout => 1, Port => 10002);
+my $t = new Net::Telnet (Timeout => 1, Port => 4000);
 
-$t->open('rs232nmr.online.a2.kph');
+$t->open('10.32.165.31');
 $t->binmode(1);
 $t->telnetmode(0);
 $t->timeout(0);
-
+#$t->dump_log(1);
 my $out = "READMAGFIELD\r";
 foreach my $c (split(//,$out)) {
   $t->put($out);
   usleep(100e3);
 }
 my $val = $t->get(Errmode => 'return');
+# the device not always responds...so just exit then
 exit unless defined $val;
 $val = substr($val,0,8); # extract field, other chars are not interesting
 
