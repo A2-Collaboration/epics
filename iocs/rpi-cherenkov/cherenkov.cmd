c5fd7bb468e5a9884a91d9b4567b3329b09f7cd8
Daniel Albuquerque <danalbu27@gmail.com>
2015-07-24 12:34:53 +0000

    Adding the required EPICS files for reading Cherenkov Gas system readouts.

--- iocs/rpi-cherenkov/cherenkov.cmd
+++ iocs/rpi-cherenkov/cherenkov.cmd
@@ -1,18 +1,23 @@
-dbLoadDatabase "/opt/epics/modules/stream/streamDevice/streamApp/O.Common/streamApp.dbd"
+dbLoadDatabase "/opt/epics/modules/streamdevice/dbd/streamApp.dbd"
 
 streamApp_registerRecordDeviceDriver
 
 epicsEnvSet "STREAM_PROTOCOL_PATH", "/opt/epics/iocs/rpi-cherenkov/"
 
-drvAsynSerialPortConfigure ("TAGG:EPT", "/dev/serial/by-id/usb-")
-asynSetOption ("TAGG:EPT", 0, "baud", "9600")
-asynSetOption ("TAGG:EPT", 0, "bits", "8")
-asynSetOption ("TAGG:EPT", 0, "parity", "none")
-asynSetOption ("TAGG:EPT", 0, "stop", "1")
+#drvAsynSerialPortConfigure ("CHERENKOV", "/dev/ttyUSB0")
+
+drvAsynSerialPortConfigure ("CHERENKOV", "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A103R38C-if00-port0")
+
+
+asynSetOption ("CHERENKOV", 0, "baud", "38400")
+asynSetOption ("CHERENKOV", 0, "bits", "8")
+asynSetOption ("CHERENKOV", 0, "parity", "none")
+asynSetOption ("CHERENKOV", 0, "stop", "1")
 
 dbLoadRecords "cherenkov.db","PROTO=cherenkov_serial.proto,DEV=CHERENKOV"
 
 iocInit
 
 #var streamDebug 1
+
 
