dce2c94263abd2928cc7a4c84b5efabb36494c86
Martin Wolfes <m.wolfes@t-online.de>
2016-01-21 10:25:03 +0000

    [beamtable] fixed ioc

--- iocs/rpi-beamtable/beamtable.cmd
+++ iocs/rpi-beamtable/beamtable.cmd
@@ -4,15 +4,15 @@ streamApp_registerRecordDeviceDriver
 
 epicsEnvSet "STREAM_PROTOCOL_PATH", "/opt/epics/iocs/rpi-beamtable/"
 
-#drvAsynSerialPortConfigure ("BEAMTABLE", "/dev/ttyUSB0")
+#drvAsynSerialPortConfigure ("BEAM:TABLE", "/dev/ttyUSB0")
 
-drvAsynSerialPortConfigure ("BEAMTABLE", "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9014YWT-if00-port0")
+drvAsynSerialPortConfigure ("BEAM:TABLE", "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9014YWT-if00-port0")
 
 
-asynSetOption ("BEAMTABLE", 0, "baud", "57600")
-asynSetOption ("BEAMTABLE", 0, "bits", "8")
-asynSetOption ("BEAMTABLE", 0, "parity", "none")
-asynSetOption ("BEAMTABLE", 0, "stop", "1")
+asynSetOption ("BEAM:TABLE", 0, "baud", "57600")
+asynSetOption ("BEAM:TABLE", 0, "bits", "8")
+asynSetOption ("BEAM:TABLE", 0, "parity", "none")
+asynSetOption ("BEAM:TABLE", 0, "stop", "1")
 
 dbLoadRecords "beamtable.db","PROTO=beamtable_serial.proto,DEV=BEAM:TABLE"
 
