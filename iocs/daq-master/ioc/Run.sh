1c7893945ec2842f52d85bb20098f4b570c2c085
Oliver Steffen <steffen@kph.uni-mainz.de>
2016-05-18 09:52:20 +0200

    Added APPT records to DAQ master

--- iocs/daq-master/ioc/Run.sh
+++ iocs/daq-master/ioc/Run.sh
@@ -2,4 +2,4 @@
 mkdir -p /tmp/ioc
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-softIoc -d general.db -d MWPC.db -d CB.db -d Scratch.db -d NMR.db -d PairSpec.db -d Target.db -d beamcontrol.db dump.cmd
+softIoc -d general.db -d MWPC.db -d CB.db -d Scratch.db -d NMR.db -d PairSpec.db -d Target.db -d beamcontrol.db -d APPT.db dump.cmd
